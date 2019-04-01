26253306d74bc5ad7fa2a0ee686c09caf6d957d9
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-04-01 14:54:05 +0200

    25.5

--- server.js
+++ server.js
@@ -3,40 +3,27 @@ var fs = require('fs');
 var bodyParser = require('body-parser')
 var app = express();
 var stringifyFile;
-
+app.use(express.static('assets'));
 app.use(bodyParser.json());
 
-app.get('/getNote', function(req, res){
-read(req, res);
+
+app.use('/store', function (req, res, next) {
+  console.log('Jestem pośrednikiem przy żądaniu do /store');
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.sendFile('/index.html');
+});
+
+app.get('/store', function (req, res) {
+  res.send('To jest sklep');
 });
 
 
-app.post('/updateNote/:note', function(req, res){
-  fs.readFile('./test.json', 'utf-8', function(err, data){
-    if (err) throw err;
-    stringifyFile = data + req.params.note;
-    });
-  fs.writeFile('./test.json', stringifyFile, function(err){
-    if (err) throw err;
-    console.log('file updated');
-  })
-
-});
-
-
-function read(req, res) {
-  fs.readFile('./test.json', 'utf-8', function(err, data){
-    if (err) throw err;
-    stringifyFile = data;
-    res.send(data);
-    });
-
-}
-
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).send('nie znaleziono');
 
 })
 
-var server= app.listen(3000);
+var server = app.listen(3000);
