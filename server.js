942fd27e0336209cb03f4a6bdae8fb1d8dc9efbc
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-05-14 16:03:46 +0200

    ver 2

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+/* w updateNote writeFile powinno byc wewnatrz callbacka od readFile
+
+dodatkowo - po co Ci funkcja read? w tym wypadku czytelniej byloby ją pominąć, ewentualnie zapisac
+app.get('/getNote', read); */
+
 var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser')
@@ -6,37 +11,32 @@ var stringifyFile;
 
 app.use(bodyParser.json());
 
-app.get('/getNote', function(req, res){
-read(req, res);
-});
+app.get('/getNote', read)
 
-
-app.post('/updateNote/:note', function(req, res){
-  fs.readFile('./test.json', 'utf-8', function(err, data){
+app.post('/updateNote/:note', function (req, res) {
+  fs.readFile('./test.json', 'utf-8', function (err, data) {
     if (err) throw err;
     stringifyFile = data + req.params.note;
-    });
-  fs.writeFile('./test.json', stringifyFile, function(err){
-    if (err) throw err;
-    console.log('file updated');
-  })
-
+    write(stringifyFile)
+  });
 });
 
-
 function read(req, res) {
-  fs.readFile('./test.json', 'utf-8', function(err, data){
+  fs.readFile('./test.json', 'utf-8', function (err, data) {
     if (err) throw err;
     stringifyFile = data;
     res.send(data);
-    });
-
+  });
 }
 
+function write(stringifyFile) {
+  fs.writeFile('./test.json', stringifyFile, function (err) {
+    if (err) throw err;
+  })
+}
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).send('nie znaleziono');
-
 })
 
-var server= app.listen(3000);
+var server = app.listen(3000);
